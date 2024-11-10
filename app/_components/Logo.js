849dd9ab0259c4706238b462786ee50db22a2862
Ashab-Asir <ashabasir101@gmail.com>
2024-11-10 14:28:09 +0600

    optimizing image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,14 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-3 sm:gap-4 z-10">
-      <img
+      <Image
         src="/logo4.png"
-        height="48"
-        width="48"
+        height="80"
+        width="80"
+        quality={100}
         alt="The Wild Oasis logo"
-        className="h-10 w-10 sm:h-12 sm:w-12"
+        className="h-15 w-15 sm:h-15 sm:w-15"
       />
       <span className="text-lg sm:text-xl font-semibold text-primary-100">
         The Cozy Cabins
