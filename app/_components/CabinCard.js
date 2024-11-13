e3b6ecd5aec054f72c5413187d44c00c209441ce
Ashab-Asir <ashabasir101@gmail.com>
2024-11-13 10:29:40 +0600

    fetching and displaying cabin list

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,4 +1,5 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
@@ -11,31 +12,32 @@ function CabinCard({ cabin }) {
     /imageUrl\s*\+\s*"([^"]+)"/g,
     (_, filename) => `${baseUrl}${filename}`
   );
-  return (
-    <div className="flex border-primary-800 border">
-      <img
-        src={newImg}
-        alt={`Cabin ${name}`}
-        className="flex-1 border-r border-primary-800"
-      />
 
-      <div className="flex-grow">
-        <div className="pt-5 pb-4 px-7 bg-primary-950">
-          <h3 className="text-accent-500 font-semibold text-2xl mb-3">
+  return (
+    <div className="flex flex-col md:flex-row border border-primary-800 rounded-lg overflow-hidden bg-primary-950 shadow-lg">
+      <div className="relative h-48 md:h-60 lg:h-68  md:w-1/2 lg:w-1/3">
+        <Image
+          src={newImg}
+          fill
+          alt={`Cabin ${name}`}
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col justify-between flex-grow p-4">
+        <div>
+          <h3 className="text-accent-500 font-semibold text-xl lg:text-2xl mb-2">
             Cabin {name}
           </h3>
-
           <div className="flex gap-3 items-center mb-2">
             <UsersIcon className="h-5 w-5 text-primary-600" />
-            <p className="text-lg text-primary-200">
+            <p className="text-sm lg:text-lg text-primary-200">
               For up to <span className="font-bold">{maxCapacity}</span> guests
             </p>
           </div>
-
           <p className="flex gap-3 justify-end items-baseline">
             {discount > 0 ? (
               <>
-                <span className="text-3xl font-[350]">
+                <span className="text-xl lg:text-3xl font-[350]">
                   ${regularPrice - discount}
                 </span>
                 <span className="line-through font-semibold text-primary-600">
@@ -43,16 +45,20 @@ function CabinCard({ cabin }) {
                 </span>
               </>
             ) : (
-              <span className="text-3xl font-[350]">${regularPrice}</span>
+              <span className="text-xl lg:text-3xl font-[350]">
+                ${regularPrice}
+              </span>
             )}
-            <span className="text-primary-200">/ night</span>
+            <span className="text-primary-200 text-sm lg:text-base">
+              / night
+            </span>
           </p>
         </div>
 
-        <div className="bg-primary-950 border-t border-t-primary-800 text-right">
+        <div className="text-right mt-4">
           <a
             href={`/cabins/${id}`}
-            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
+            className="inline-block py-2 px-4 border border-primary-800 rounded hover:bg-accent-600 transition-all hover:text-primary-900 text-sm lg:text-base"
           >
             Details & reservation &rarr;
           </a>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -5,15 +5,15 @@ import { getCabins } from "../_lib/data-service";
 export const metadata = {
   title: "Cabins",
 };
+
 export default async function Page() {
-  // CHANGE
   const cabins = await getCabins();
   return (
-    <div>
-      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+    <div className="p-4 md:p-8">
+      <h1 className="text-2xl md:text-4xl mb-4 md:mb-5 text-accent-400 font-medium">
         Our Luxury Cabins
       </h1>
-      <p className="text-primary-200 text-lg mb-10">
+      <p className="text-primary-200 text-base md:text-lg mb-6 md:mb-10">
         Cozy and luxurious cabins nestled in the heart of the Bandarban Hills or
         Sajek Valley. Picture yourself waking up to stunning mountain views,
         exploring nearby forests, or unwinding in a private hot tub under a
@@ -23,7 +23,7 @@ export default async function Page() {
       </p>
 
       {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
           {cabins.map((cabin) => (
             <CabinCard cabin={cabin} key={cabin.id} />
           ))}
